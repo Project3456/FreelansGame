5bc9e9727a017f041f8b7562a47f0c29f22e54f9
Project3456 <padal1924@gmail.com>
2025-06-05 22:42:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+﻿import random 
+
+class Freelancer:
+    def __init__(self,name,money,time,orders):
+        self.name = name
+        self.money = money
+        self.time = time
+        self.orders = {}
+        
+    def show_status(self):
+        return f"Фрилансер: {self.name}, Бюджет: ${self.money},Время: {self.time},Заказы: {self.orders} "
+    
+freelans = Freelancer("Andrey",50,10,"No")
+print(freelans.show_status())
+        
